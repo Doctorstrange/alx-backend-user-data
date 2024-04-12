c4d42784756e7eadb0b49d42e9d2744016dc1a5c
doctorstrange <doctorstrange@gmail.com>
2024-04-12 01:57:00 +0000

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
-""" filter_datum that returns the log message obfuscated """
+""" accept a list of strings fields constructor argument. """
 import re
 from typing import List
 import logging
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """ accept a list of strings fields constructor argument.
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: list):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        """ accept a list of strings fields constructor argument. """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
