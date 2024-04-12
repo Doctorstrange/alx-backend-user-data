70bb89d4bdf3e63f68b3cdf3e9bf7c4874b87a69
doctorstrange <doctorstrange@gmail.com>
2024-04-12 01:51:38 +0000

    from typing import List

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 """ filter_datum that returns the log message obfuscated """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    pattern = re.compile(r'(\b' + '|'.join(fields) + r')=')
-    return pattern.sub(r'\1=' + redaction + separator, message)
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Returns regex obfuscated log messages """
+    for value in fields:
+        message = re.sub(f'{value}=(.*?){separator}',
+                         f'{value}={redaction}{separator}', message)
+    return message
