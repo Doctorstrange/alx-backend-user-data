7e6b1d5a6a26ed05b3f43177662db8451a1dd283
doctorstrange <doctorstrange@gmail.com>
2024-04-17 00:59:16 +0000

    7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -26,7 +26,6 @@ class BasicAuth(Auth):
                 return field_match.group('token')
         return None
 
-
     def decode_base64_authorization_header(
             self,
             base64_authorization_header: str,
