b8142a946472375704f226ff58a2a8f7d7496f9e
doctorstrange <doctorstrange@gmail.com>
2024-04-18 21:03:04 +0000

    7. New view for Session Authentication

---
+++ 0x02-Session_authentication/api/v1/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" 7. New view for Session Authentication
+"""
+
+from api.v1.views.index import *
+from api.v1.views.users import *
+from api.v1.views.session_auth import *
