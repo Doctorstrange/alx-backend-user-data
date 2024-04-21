5e98aa9e8348fd41e0d3015a42ab3e5f4763e895
doctorstrange <doctorstrange@gmail.com>
2024-04-21 02:02:31 +0000

    0. Simple-basic-API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,25 +3,17 @@
 Route module for the API
 """
 from os import getenv
-from typing import Tuple
 from api.v1.views import app_views
 from flask_cors import (CORS, cross_origin)
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
-auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
@@ -44,26 +36,27 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.before_request
-def handle_request():
+def authenticate_user():
+    """handler before_request
     """
-    handler before_request
-    """
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    auth_header = auth.authorization_header(request)
-    if auth_header is None:
-        abort(401)
-    user = auth.current_user(request)
-    if user is None:
-        abort(403)
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,20 +9,19 @@ import fnmatch
 class Auth:
     """Authentication class.
     """
-        if not path:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path and excluded_paths
+        """
+        if path is None:
             return True
 
-        if not excluded_paths:
+        if excluded_paths is None or not excluded_paths:
             return True
 
-        path = path.rstrip("/")
-        for excluded_path in excluded_paths:
-            if excluded_path.endswith("*") and \
-                    path.startswith(excluded_path[:-1]):
+        for ex_path in excluded_paths:
+            if fnmatch.fnmatch(path, ex_path):
                 return False
 
-            elif path == excluded_path.rstrip("/"):
-                return False
         return True
 
     def authorization_header(self, request=None) -> str:
