7a036ffc8e155d18226601d6e2b31cb28ed129e9
doctorstrange <doctorstrange@gmail.com>
2024-04-25 20:25:56 +0000

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -20,3 +20,23 @@ def _hash_password(password: str) -> bytes:
     """ takes in a password string arguments and returns bytes
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """mandatory email and password string arguments
+        and return a User object
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+        hashed_password = _hash_password(password)
+        user = self._db.add_user(email, hashed_password)
+        return user
