c7712eb44c4157a3bc3292eef92c2ae92077382e
doctorstrange <doctorstrange@gmail.com>
2024-04-25 21:22:00 +0000

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,3 +41,18 @@ class Auth:
         hashed_password = _hash_password(password)
         user = self._db.add_user(email, hashed_password)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ expect email and password required
+        arguments and return a boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                password = password.encode('utf-8')
+                hashed_password = user.hashed_password
+                if bcrypt.checkpw(password, hashed_password):
+                    return True
+        except NoResultFound:
+            return False
+        return False
