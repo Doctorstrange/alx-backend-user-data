9f74c61785cc5e74eb02bff7cbb112a818140d6b
doctorstrange <doctorstrange@gmail.com>
2024-04-25 22:52:43 +0000

    9. Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -22,6 +22,13 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """ should return a string representation
+    of a new UUID. Use the uuid module
+    """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -56,9 +63,3 @@ class Auth:
         except NoResultFound:
             return False
         return False
-
-    def _generate_uuid() -> str:
-        """ should return a string representation
-        of a new UUID. Use the uuid module
-        """
-        return str(uuid4())
