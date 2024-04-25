badb32c3828774458a2ceaf888490450caf2218a
doctorstrange <doctorstrange@gmail.com>
2024-04-25 21:04:28 +0000

    7. Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,3 +20,17 @@ def index() -> str:
     and use flask.jsonify to return a JSON
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """implements the POST /users route.
+    Import the Auth object and instantiate
+    it at the root of the module
+    """
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
