0289c6db96e37c8391cdd59a154f2ccc906c502d
doctorstrange <doctorstrange@gmail.com>
2024-04-26 00:50:45 +0000

    16. Generate reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,3 +70,14 @@ def profile() -> str:
     if user is None:
         abort(403)
     return jsonify({"email": user.email})
+
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ It take an email string argument and returns a string
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": reset_token})
